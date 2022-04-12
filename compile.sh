9319c46f35edf389d4edd67a06cfc9ab0938d108
hilbert9221 <hilbert9221@gmail.com>
2022-04-12 10:10:42 +0800

    auto-create classes

--- compile.sh
+++ compile.sh
@@ -1,3 +1,8 @@
+if [ ! -d "classes" ];
+then
+    mkdir classes
+fi
+
 javac -classpath "/home/rotopia/Downloads/NetLogo-6.2.1/app/netlogo-6.2.1.jar" -d classes src/com/test/*.java
 
 jar cvfm test.jar MANIFEST.MF -C classes .
